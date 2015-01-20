c0c3586eda791fe365af3d29e4c5f71155e04203
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-01-20 16:45:22 +0100

    preparing for an upload to pypi

--- setup.py
+++ setup.py
@@ -4,11 +4,25 @@ Dictionaries that survive one run
 """
 import setuptools
 
+version="0.0.1"
+
 setuptools.setup(
     name="persistentdicts",
-    version='0.0.1',
-    long_description=__doc__,
+    version=version,
+    description=__doc__,
+    author="Christophe-Marie Duquesne",
+    author_email ="chmd@chmd.fr",
+    url="https://github.com/chmduquesne/persistentdicts",
+    download_url="https://github.com/chmduquesne/persistentdicts/tarball/%s" % (version),
+    keywords=["database", "interface", "adapter"]
     packages=["persistentdicts"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        ]
     install_requires=[
         "kyotocabinet >= 1.9"
         ]
