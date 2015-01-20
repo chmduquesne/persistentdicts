1deeda8bde9c7684a8561ee7d78e872ff21ecc9a
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-01-20 09:53:36 +0100

    Adding in dependencies

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Dictionaries that survive one run
+"""
+import setuptools
+
+setuptools.setup(
+    name="persistentdicts",
+    version='0.0.1',
+    long_description=__doc__,
+    packages=["persistentdicts"],
+    install_requires=[
+        "sqlite3"
+        ]
+)
