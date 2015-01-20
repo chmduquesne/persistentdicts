a18428356fe3833717dda87b7851c1cbba0d718f
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-01-20 16:58:50 +0100

    Correcting typos

--- setup.py
+++ setup.py
@@ -4,25 +4,25 @@ Dictionaries that survive one run
 """
 import setuptools
 
-version="0.0.1"
+version="0.0.2"
 
 setuptools.setup(
     name="persistentdicts",
+    packages=["persistentdicts"],
     version=version,
     description=__doc__,
     author="Christophe-Marie Duquesne",
     author_email ="chmd@chmd.fr",
     url="https://github.com/chmduquesne/persistentdicts",
-    download_url="https://github.com/chmduquesne/persistentdicts/tarball/%s" % (version),
-    keywords=["database", "interface", "adapter"]
-    packages=["persistentdicts"],
+    download_url="https://github.com/chmduquesne/persistentdicts/archive/%s.tar.gz" % (version),
+    keywords=["database", "interface", "adapter"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        ]
+        ],
     install_requires=[
         "kyotocabinet >= 1.9"
         ]
