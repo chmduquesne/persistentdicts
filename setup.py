910ebadf0f47dc75e5fe782dd60ee7ef9c8588dc
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-01-22 12:04:28 +0100

    adding version number

--- setup.py
+++ setup.py
@@ -4,7 +4,22 @@ Dictionaries that survive one run
 """
 import setuptools
 
-version="0.0.1"
+# We use semantic versioning http://semver.org/.
+#
+# Summary: Given a version number MAJOR.MINOR.PATCH, we increment the:
+#
+#     MAJOR for incompatible API changes,
+#     MINOR for added functionality in a backwards-compatible manner
+#     PATCH for backwards-compatible bug fixes.
+#
+# Additionally, for dev, release candidates, and post releases, we respect
+# https://www.python.org/dev/peps/pep-0440/ by suffixing accordingly.
+
+version = open("version").read()
+if version.contains("dev"):
+    github_download = "master"
+else:
+    github_download = version
 
 setuptools.setup(
     name="persistentdicts",
@@ -13,7 +28,7 @@ setuptools.setup(
     author="Christophe-Marie Duquesne",
     author_email ="chmd@chmd.fr",
     url="https://github.com/chmduquesne/persistentdicts",
-    download_url="https://github.com/chmduquesne/persistentdicts/tarball/%s" % (version),
+    download_url="https://github.com/chmduquesne/persistentdicts/archive/%s.tar.gz" % (github_download),
     keywords=["database", "interface", "adapter"]
     packages=["persistentdicts"],
     classifiers=[
